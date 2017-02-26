cdf2fea0f858c2f2b521e2ca99ddfe99d49377bc
smarzioni <simone.marzioni88@gmail.com>
2017-02-26 19:53:09 +0100

    Folder Reorganization 2

--- Joshi/Appendices/Normals/Normals/Normals.h
+++
@@ -1,9 +0,0 @@
-#ifndef NORMALS_H
-#define NORMALS_H
-//Nomral density, cumulative and inverse cumulative functions.
-
-double NormalDensity(double x);
-double CumulativeNormal(double x);
-double InverseCumulativeNormal(double x);
-
-#endif // !NORMALS_H
--- Joshi/Chapter 1/Exercise2/Exercise2/Random1.h
+++
@@ -1,7 +0,0 @@
-#ifndef RANDOM1_H
-#define RANDOM1_H
-
-double GetOneGaussianBySummation();
-double GetOneGaussianByBoxMuller();
-#endif
-
